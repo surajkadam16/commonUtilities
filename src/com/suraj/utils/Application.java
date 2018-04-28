8c17b858d4ef1e28a3870cdc9fda8d87e013274a
Nakul <Nakul@192.168.0.4>
2018-04-28 00:20:23 -0500

    change input

--- src/com/suraj/utils/Application.java
+++ src/com/suraj/utils/Application.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class Application {
 
 	public static void main(String[] args) {
-		Map<Character,Double> countMap = StringUtils.getCharacterCount("suraaj");
+		Map<Character,Double> countMap = StringUtils.getCharacterCount("suraajjj");
 		
 		countMap.entrySet().stream().forEach(x-> {
 			System.out.println("Character/Count : " + x.getKey() + "/" + x.getValue());
